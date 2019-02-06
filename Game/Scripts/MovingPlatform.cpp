69b7df54470799bdf041e99f273033ad1ef69909
RedSlimeKing <DevonALy@outlook.com>
2019-02-06 17:16:38 -0500

    Added moving platforms

---
+++ Game/Scripts/MovingPlatform.cpp
@@ -0,0 +1,66 @@
+#include "MovingPlatform.h"
+
+#include <Object/GameObject.h> 
+
+using namespace GamePackage;
+
+MovingPlatform::MovingPlatform() : direction(PizzaBox::Vector3(1, 0, 0)), directionSpeed(1.0f), distance(1.0f) {
+
+}
+
+void MovingPlatform::OnStart() {
+	origin = gameObject->GetTransform()->GetPosition().x;
+	useSpeed = -directionSpeed;
+}
+
+void MovingPlatform::Update(const float deltaTime_) {
+	if (direction.x > 0) {
+		if (origin - gameObject->GetTransform()->GetPosition().x > distance) {
+			//flip direction
+			useSpeed = directionSpeed;
+		}
+		else if (origin - gameObject->GetTransform()->GetPosition().x < -distance) {
+			//flip direction
+			useSpeed = -directionSpeed;
+		}
+		gameObject->GetTransform()->Translate(PizzaBox::Vector3(useSpeed * deltaTime_, 0, 0));
+	}
+	else if (direction.y > 0) {
+		if (origin - gameObject->GetTransform()->GetPosition().y > distance) {
+			//flip direction
+			useSpeed = directionSpeed;
+		}
+		else if (origin - gameObject->GetTransform()->GetPosition().y < -distance) {
+			//flip direction
+			useSpeed = -directionSpeed;
+		}
+		gameObject->GetTransform()->Translate(PizzaBox::Vector3(0, useSpeed * deltaTime_, 0));
+	}
+	else if (direction.z > 0) {
+		if (origin - gameObject->GetTransform()->GetPosition().z > distance) {
+			//flip direction
+			useSpeed = directionSpeed;
+		}
+		else if (origin - gameObject->GetTransform()->GetPosition().z < -distance) {
+			//flip direction
+			useSpeed = -directionSpeed;
+		}
+		gameObject->GetTransform()->Translate(PizzaBox::Vector3(0, 0, useSpeed * deltaTime_));
+	}
+}
+
+void MovingPlatform::OnDestroy() {
+
+}
+
+void MovingPlatform::SetDirection(PizzaBox::Vector3 dir) {
+	direction = dir;
+}
+
+void MovingPlatform::SetDirectionSpeed(float speed_) {
+	directionSpeed = speed_;
+}
+
+void MovingPlatform::SetDistance(float dis) {
+	distance = dis;
+}
---
+++ Game/Scripts/MovingPlatform.h
@@ -0,0 +1,31 @@
+#ifndef MOVING_PLATFORM_H
+#define MOVING_PLATFORM_H
+
+#include <Script/Script.h>
+#include <Math/Vector.h>
+
+namespace GamePackage {
+	class MovingPlatform : public PizzaBox::Script {
+	public:
+		MovingPlatform();
+
+		virtual void OnStart() override;
+		virtual void Update(const float deltaTime_) override;
+		virtual void OnDestroy() override;
+
+		//Setters
+		void SetDirection(PizzaBox::Vector3 dir);
+		void SetDirectionSpeed(float speed_);
+		void SetDistance(float dis);
+	private:
+		//these are for setup
+		PizzaBox::Vector3 direction;
+		float directionSpeed;
+		float distance;
+		//These are for holding info
+		float useSpeed;
+		float origin;
+	};
+}
+
+#endif //!MOVING_PLATFORM_H
