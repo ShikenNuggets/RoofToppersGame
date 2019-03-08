621186989e4f313d76fce6daf2cb088185ddb3b0
Carter Rennick <carter.rennick@gmail.com>
2019-03-08 16:23:04 -0500

    Updated UI

--- Game/Game.cpp
+++ Game/Game.cpp
@@ -23,14 +23,14 @@ Game::~Game(){
 }
 
 bool Game::Initialize(){ 
-	PizzaBox::RenderEngine::SetWindowResolution(1920, 1080);
-	PizzaBox::RenderEngine::SetWindowBorderless(true);
+	PizzaBox::RenderEngine::SetWindowResolution(1280, 720);
+	PizzaBox::RenderEngine::SetWindowBorderless(false);
 	PizzaBox::RenderEngine::SetVSYNC(PizzaBox::Window::VSYNC::Off);
 	PizzaBox::Time::SetFrameRate(0);
 
 	//Audio Channels
 	//PizzaBox::AudioManager::CreateVolumeChannel("Music");
-	/*
+	
 	//Logo
 	PizzaBox::UISet* uiSet = new PizzaBox::UISet("LogoSet");
 	PizzaBox::ImageUI* logo = new PizzaBox::ImageUI("Logo", "LogoHDImage", PizzaBox::Rect(0.0f, 0.0f, 1.0f, 1.0f));
@@ -39,14 +39,16 @@ bool Game::Initialize(){
 
 	//MainMenuSet
 	uiSet = new PizzaBox::UISet("MainMenuSet");
-	uiSet->elements.push_back(new PizzaBox::TextUI("GameName", PizzaBox::Rect(0.35, 0.5, 0.1, 0.125), "Protype Name", "ArialFont"));
-	uiSet->elements.push_back(new PizzaBox::ButtonUI("PlayButton", PizzaBox::Rect(0.425, 0.35, 0.15, 0.125)));
-	uiSet->elements.push_back(new PizzaBox::ButtonUI("QuitButton", PizzaBox::Rect(0.425, 0.2, 0.15, 0.125)));
+	uiSet->elements.push_back(new PizzaBox::TextUI("GameName", PizzaBox::Rect(0.325f, 0.5f, 0.15f, 0.175f), "Roof Toppers", "ArialFont"));
+	uiSet->elements.push_back(new PizzaBox::ButtonUI("PlayButton", PizzaBox::Rect(0.425f, 0.35f, 0.15f, 0.125f)));
+	uiSet->elements.push_back(new PizzaBox::TextUI("PlayText", PizzaBox::Rect(0.36f, 0.24f, 0.15f, 0.125f), "Play", "ArialFont"));
+	uiSet->elements.push_back(new PizzaBox::ButtonUI("QuitButton", PizzaBox::Rect(0.425f, 0.2f, 0.15f, 0.125f)));
+	uiSet->elements.push_back(new PizzaBox::TextUI("QuitText", PizzaBox::Rect(0.36f, 0.15f, 0.15f, 0.125f), "Quit", "ArialFont"));
 	PizzaBox::UIManager::AddSet(uiSet);
-	*/
+	
 	//Add scenes to the SceneManager 
 	//PizzaBox::SceneManager::AddScene(new LogoScene());
-	//PizzaBox::SceneManager::AddScene(new MainMenuScene());
+	PizzaBox::SceneManager::AddScene(new MainMenuScene());
 	PizzaBox::SceneManager::AddScene(new Level1());
 
 	return true;
