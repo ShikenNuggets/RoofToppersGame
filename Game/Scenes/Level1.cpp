60b05a47f93db93de43d3067656bdd5e6eff874c
Carter Rennick <carter.rennick@gmail.com>
2019-03-11 17:39:35 -0400

    Fixed scaling on everything

--- Game/Scenes/Level1.cpp
+++ Game/Scenes/Level1.cpp
@@ -34,10 +34,10 @@ Level1::~Level1() {
 }
 
 bool Level1::Initialize() {
-	// SkyBox
+	//SkyBox
 	SetSky(new PizzaBox::SkyBox("LakeSkybox", "SkyBoxShader", 68000.0f));
 
-	// Camera
+	//Camera
 	PizzaBox::GameObject* mainCamera = CreateObject<PizzaBox::GameObject>(PizzaBox::Vector3(0.0f, 55.0f, 80.0f), PizzaBox::Euler(-15.0f, 0.0f, 0.0f));
 	auto cam = new PizzaBox::Camera(PizzaBox::ViewportRect::fullScreen, PizzaBox::Camera::RenderMode::Perspective);
 	mainCamera->AddComponent(cam);
@@ -46,12 +46,12 @@ bool Level1::Initialize() {
 	//mainCamera->AddComponent(new PizzaBox::AudioListener());
 	//mainCamera->AddComponent(new PizzaBox::AudioSource("GameplayMusic", PizzaBox::AudioSource::SoundType::_2D, "Music"));
 
-	// Directional Light
+	//Directional Light
 	auto dirLight = CreateObject<PizzaBox::GameObject>(PizzaBox::Vector3(), PizzaBox::Euler(-35.0f, 12.0f, 0.0f));
 	dirLight->AddComponent(new PizzaBox::DirectionalLight(2.0f));
 
-	// Player 
-	PizzaBox::GameObject* Player = CreateObject<PizzaBox::GameObject>(PizzaBox::Vector3(0.0f, 60.0f, 0.0f), PizzaBox::Euler(0.0f, 180.0f, 0.0f), PizzaBox::Vector3(0.05f, 0.05f, 0.05f));
+	//Player 
+	PizzaBox::GameObject* Player = CreateObject<PizzaBox::GameObject>(PizzaBox::Vector3(0.0f, 60.0f, 0.0f), PizzaBox::Euler(0.0f, 180.0f, 0.0f), PizzaBox::Vector3(0.01f, 0.01f, 0.01f));
 	Player->SetTag("Player");
 	PlayerAnimator* animator = new PlayerAnimator();
 	std::vector<PizzaBox::MeshMaterial*> materials;
@@ -76,8 +76,8 @@ bool Level1::Initialize() {
 	//grapplePoint->AddComponent(new PizzaBox::MeshRender("SphereModel", new PizzaBox::ColorMaterial(PizzaBox::Color::Yellow)));
 	//grapplePoint->AddComponent(new GrapplePoint(35.0f));
 
-	// Test Static platfrom
-	PizzaBox::GameObject* platform = CreateObject<PizzaBox::GameObject>(PizzaBox::Vector3(0.0f, -5.0f, 0.0f), PizzaBox::Euler(), PizzaBox::Vector3(50.0f, 50.0f, 50.0f));
+	//Test Static platfrom
+	PizzaBox::GameObject* platform = CreateObject<PizzaBox::GameObject>(PizzaBox::Vector3(0.0f, -5.0f, 0.0f), PizzaBox::Euler(), PizzaBox::Vector3(20.0f, 100.0f, 20.0f));
 	platform->SetTag("Platform");
 	auto texMat = new PizzaBox::TexturedMaterial("ConcreteTexture", false, "", "", 32.0f, 20.0f);
 	platform->AddComponent(new PizzaBox::MeshRender("CubeModel", texMat));
@@ -88,7 +88,7 @@ bool Level1::Initialize() {
 	platform->AddComponent(rb2);
 	platform->SetStatic(true);
 
-	PizzaBox::GameObject* platform2 = CreateObject<PizzaBox::GameObject>(PizzaBox::Vector3(0.0f, 40.0f, -150.0f), PizzaBox::Euler(), PizzaBox::Vector3(30.0f, 2.0f, 30.0f));
+	PizzaBox::GameObject* platform2 = CreateObject<PizzaBox::GameObject>(PizzaBox::Vector3(0.0f, 40.0f, -60.0f), PizzaBox::Euler(), PizzaBox::Vector3(12.0f, 4.0f, 12.0f));
 	platform2->SetTag("Platform");
 	platform2->AddComponent(new PizzaBox::MeshRender("CubeModel", new PizzaBox::ColorMaterial(PizzaBox::Color::Red)));
 	//platform2->AddComponent(new PizzaBox::Collider(platform2->GetTransform()->GlobalScale()));
@@ -98,7 +98,7 @@ bool Level1::Initialize() {
 	platform2->AddComponent(rb3);
 	platform2->SetStatic(true);
 
-	PizzaBox::GameObject* platform3 = CreateObject<PizzaBox::GameObject>(PizzaBox::Vector3(0.0f, -5.0f, -300.0f), PizzaBox::Euler(), PizzaBox::Vector3(50.0f, 50.0f, 50.0f));
+	PizzaBox::GameObject* platform3 = CreateObject<PizzaBox::GameObject>(PizzaBox::Vector3(0.0f, -5.0f, -120.0f), PizzaBox::Euler(), PizzaBox::Vector3(20.0f, 100.0f, 20.0f));
 	platform3->SetTag("Platform");
 	texMat = new PizzaBox::TexturedMaterial("ConcreteTexture", false, "", "", 32.0f, 20.0f);
 	platform3->AddComponent(new PizzaBox::MeshRender("CubeModel", texMat));
@@ -109,7 +109,7 @@ bool Level1::Initialize() {
 	platform3->AddComponent(rb4);
 	platform3->SetStatic(true);
 
-	PizzaBox::GameObject* platform4 = CreateObject<PizzaBox::GameObject>(PizzaBox::Vector3(0.0f, 40.0f, -350.0f), PizzaBox::Euler(), PizzaBox::Vector3(30.0f, 2.0f, 80.0f));
+	PizzaBox::GameObject* platform4 = CreateObject<PizzaBox::GameObject>(PizzaBox::Vector3(0.0f, 40.0f, -170.0f), PizzaBox::Euler(), PizzaBox::Vector3(12.0f, 4.0f, 32.0f));
 	platform4->SetTag("Platform");
 	platform4->AddComponent(new PizzaBox::MeshRender("CubeModel", new PizzaBox::ColorMaterial(PizzaBox::Color::Red)));
 	//platform2->AddComponent(new PizzaBox::Collider(platform2->GetTransform()->GlobalScale()));
@@ -119,7 +119,7 @@ bool Level1::Initialize() {
 	platform4->AddComponent(rb5);
 	platform4->SetStatic(true);
 
-	PizzaBox::GameObject* platform5 = CreateObject<PizzaBox::GameObject>(PizzaBox::Vector3(0.0f, -5.0f, -450.0f), PizzaBox::Euler(/*-25.0f*/), PizzaBox::Vector3(50.0f, 50.0f, 50.0f));
+	PizzaBox::GameObject* platform5 = CreateObject<PizzaBox::GameObject>(PizzaBox::Vector3(0.0f, -5.0f, -270.0f), PizzaBox::Euler(/*-25.0f*/), PizzaBox::Vector3(20.0f, 100.0f, 20.0f));
 	platform5->SetTag("Platform");
 	texMat = new PizzaBox::TexturedMaterial("ConcreteTexture", false, "", "", 32.0f, 20.0f);
 	platform5->AddComponent(new PizzaBox::MeshRender("CubeModel", texMat));
@@ -130,7 +130,7 @@ bool Level1::Initialize() {
 	platform5->AddComponent(rb6);
 	platform5->SetStatic(true);
 
-	PizzaBox::GameObject* platform6 = CreateObject<PizzaBox::GameObject>(PizzaBox::Vector3(0.0f, 40.0f, -535.0f), PizzaBox::Euler(), PizzaBox::Vector3(20.0f, 2.0f, 20.0f));
+	PizzaBox::GameObject* platform6 = CreateObject<PizzaBox::GameObject>(PizzaBox::Vector3(0.0f, 40.0f, -355.0f), PizzaBox::Euler(), PizzaBox::Vector3(8.0f, 4.0f, 8.0f));
 	platform6->SetTag("Platform");
 	platform6->AddComponent(new PizzaBox::MeshRender("CubeModel", new PizzaBox::ColorMaterial(PizzaBox::Color::Red)));
 	//platform2->AddComponent(new PizzaBox::Collider(platform2->GetTransform()->GlobalScale()));
@@ -140,7 +140,7 @@ bool Level1::Initialize() {
 	platform6->AddComponent(rb7);
 	platform6->SetStatic(true);
 
-	PizzaBox::GameObject* platform7 = CreateObject<PizzaBox::GameObject>(PizzaBox::Vector3(0.0f, 60.0f, -620.0f), PizzaBox::Euler(), PizzaBox::Vector3(10.0f, 2.0f, 20.0f));
+	PizzaBox::GameObject* platform7 = CreateObject<PizzaBox::GameObject>(PizzaBox::Vector3(0.0f, 60.0f, -440.0f), PizzaBox::Euler(), PizzaBox::Vector3(4.0f, 4.0f, 8.0f));
 	platform7->SetTag("Platform");
 	platform7->AddComponent(new PizzaBox::MeshRender("CubeModel", new PizzaBox::ColorMaterial(PizzaBox::Color::Red)));
 	//platform2->AddComponent(new PizzaBox::Collider(platform2->GetTransform()->GlobalScale()));
@@ -150,7 +150,7 @@ bool Level1::Initialize() {
 	platform7->AddComponent(rb8);
 	platform7->SetStatic(true);
 
-	PizzaBox::GameObject* platform8 = CreateObject<PizzaBox::GameObject>(PizzaBox::Vector3(0.0f, -5.0f, -780.0f), PizzaBox::Euler(), PizzaBox::Vector3(50.0f, 150.0f, 50.0f));
+	PizzaBox::GameObject* platform8 = CreateObject<PizzaBox::GameObject>(PizzaBox::Vector3(0.0f, -5.0f, -600.0f), PizzaBox::Euler(), PizzaBox::Vector3(20.0f, 300.0f, 20.0f));
 	platform8->SetTag("Platform");
 	texMat = new PizzaBox::TexturedMaterial("ConcreteTexture", false, "", "", 32.0f, 20.0f);
 	platform8->AddComponent(new PizzaBox::MeshRender("CubeModel", texMat));
@@ -161,7 +161,7 @@ bool Level1::Initialize() {
 	platform8->AddComponent(rb9);
 	platform8->SetStatic(true);
 
-	PizzaBox::GameObject* platform9 = CreateObject<PizzaBox::GameObject>(PizzaBox::Vector3(0.0f, 45.0f, -780.0f), PizzaBox::Euler(), PizzaBox::Vector3(80.0f, 5.0f, 80.0f));
+	PizzaBox::GameObject* platform9 = CreateObject<PizzaBox::GameObject>(PizzaBox::Vector3(0.0f, 45.0f, -600.0f), PizzaBox::Euler(), PizzaBox::Vector3(32.0f, 10.0f, 32.0f));
 	platform9->SetTag("Platform");
 	platform9->AddComponent(new PizzaBox::MeshRender("CubeModel", new PizzaBox::ColorMaterial(PizzaBox::Color::Red)));
 	//platform->AddComponent(new PizzaBox::Collider(platform->GetTransform()->GlobalScale())); 
@@ -173,72 +173,63 @@ bool Level1::Initialize() {
 
 
 	//Grapple Points
-
-	PizzaBox::GameObject* grapplePoint1 = CreateObject<PizzaBox::GameObject>(PizzaBox::Vector3(0.0f, 80.0f, -80.0f), PizzaBox::Euler(), PizzaBox::Vector3(2.0f, 2.0f, 2.0f));
+	PizzaBox::GameObject* grapplePoint1 = CreateObject<PizzaBox::GameObject>(PizzaBox::Vector3(0.0f, 60.0f, -30.0f), PizzaBox::Euler(), PizzaBox::Vector3(2.0f, 2.0f, 2.0f));
 	grapplePoint1->AddComponent(new PizzaBox::MeshRender("SphereModel", new PizzaBox::ColorMaterial(PizzaBox::Color::Yellow)));
-	grapplePoint1->AddComponent(new GrapplePoint(35.0f));
+	grapplePoint1->AddComponent(new GrapplePoint(20.0f));
 		auto child = CreateObject<PizzaBox::GameObject>(grapplePoint1, PizzaBox::Vector3(0.0f, -3.5f, 0.0f));
 		child->AddComponent(new PizzaBox::PointLight(1.0f, PizzaBox::Color::Yellow, 5.0f));
 
-	PizzaBox::GameObject* grapplePoint2 = CreateObject<PizzaBox::GameObject>(PizzaBox::Vector3(0.0f, 80.0f, -200.0f), PizzaBox::Euler(), PizzaBox::Vector3(2.0f, 2.0f, 2.0f));
+	PizzaBox::GameObject* grapplePoint2 = CreateObject<PizzaBox::GameObject>(PizzaBox::Vector3(0.0f, 60.0f, -90.0f), PizzaBox::Euler(), PizzaBox::Vector3(2.0f, 2.0f, 2.0f));
 	grapplePoint2->AddComponent(new PizzaBox::MeshRender("SphereModel", new PizzaBox::ColorMaterial(PizzaBox::Color::Yellow)));
-	grapplePoint2->AddComponent(new GrapplePoint(35.0f));
+	grapplePoint2->AddComponent(new GrapplePoint(20.0f));
 		child = CreateObject<PizzaBox::GameObject>(grapplePoint2, PizzaBox::Vector3(0.0f, -3.5f, 0.0f));
 		child->AddComponent(new PizzaBox::PointLight(1.0f, PizzaBox::Color::Yellow, 5.0f));
 
-	PizzaBox::GameObject* grapplePoint3 = CreateObject<PizzaBox::GameObject>(PizzaBox::Vector3(0.0f, 80.0f, -380.0f), PizzaBox::Euler(), PizzaBox::Vector3(2.0f, 2.0f, 2.0f));
+	PizzaBox::GameObject* grapplePoint3 = CreateObject<PizzaBox::GameObject>(PizzaBox::Vector3(0.0f, 60.0f, -120.0f), PizzaBox::Euler(), PizzaBox::Vector3(2.0f, 2.0f, 2.0f));
 	grapplePoint3->AddComponent(new PizzaBox::MeshRender("SphereModel", new PizzaBox::ColorMaterial(PizzaBox::Color::Yellow)));
-	grapplePoint3->AddComponent(new GrapplePoint(35.0f));
+	grapplePoint3->AddComponent(new GrapplePoint(20.0f));
 		child = CreateObject<PizzaBox::GameObject>(grapplePoint3, PizzaBox::Vector3(0.0f, -3.5f, 0.0f));
 		child->AddComponent(new PizzaBox::PointLight(1.0f, PizzaBox::Color::Yellow, 5.0f));
 
-	PizzaBox::GameObject* grapplePoint4 = CreateObject<PizzaBox::GameObject>(PizzaBox::Vector3(0.0f, 80.0f, -470.0f), PizzaBox::Euler(), PizzaBox::Vector3(2.0f, 2.0f, 2.0f));
+	PizzaBox::GameObject* grapplePoint4 = CreateObject<PizzaBox::GameObject>(PizzaBox::Vector3(0.0f, 60.0f, -150.0f), PizzaBox::Euler(), PizzaBox::Vector3(2.0f, 2.0f, 2.0f));
 	grapplePoint4->AddComponent(new PizzaBox::MeshRender("SphereModel", new PizzaBox::ColorMaterial(PizzaBox::Color::Yellow)));
-	grapplePoint4->AddComponent(new GrapplePoint(35.0f));
+	grapplePoint4->AddComponent(new GrapplePoint(20.0f));
 		child = CreateObject<PizzaBox::GameObject>(grapplePoint4, PizzaBox::Vector3(0.0f, -3.5f, 0.0f));
 		child->AddComponent(new PizzaBox::PointLight(1.0f, PizzaBox::Color::Yellow, 5.0f));
 
-	PizzaBox::GameObject* grapplePoint5 = CreateObject<PizzaBox::GameObject>(PizzaBox::Vector3(0.0f, 80.0f, -560.0f), PizzaBox::Euler(), PizzaBox::Vector3(2.0f, 2.0f, 2.0f));
+	PizzaBox::GameObject* grapplePoint5 = CreateObject<PizzaBox::GameObject>(PizzaBox::Vector3(0.0f, 60.0f, -180.0f), PizzaBox::Euler(), PizzaBox::Vector3(2.0f, 2.0f, 2.0f));
 	grapplePoint5->AddComponent(new PizzaBox::MeshRender("SphereModel", new PizzaBox::ColorMaterial(PizzaBox::Color::Yellow)));
-	grapplePoint5->AddComponent(new GrapplePoint(35.0f));
+	grapplePoint5->AddComponent(new GrapplePoint(20.0f));
 		child = CreateObject<PizzaBox::GameObject>(grapplePoint5, PizzaBox::Vector3(0.0f, -3.5f, 0.0f));
 		child->AddComponent(new PizzaBox::PointLight(1.0f, PizzaBox::Color::Yellow, 5.0f));
 
-	PizzaBox::GameObject* grapplePoint6 = CreateObject<PizzaBox::GameObject>(PizzaBox::Vector3(0.0f, 100.0f, -660.0f), PizzaBox::Euler(), PizzaBox::Vector3(2.0f, 2.0f, 2.0f));
+	PizzaBox::GameObject* grapplePoint6 = CreateObject<PizzaBox::GameObject>(PizzaBox::Vector3(0.0f, 60.0f, -210.0f), PizzaBox::Euler(), PizzaBox::Vector3(2.0f, 2.0f, 2.0f));
 	grapplePoint6->AddComponent(new PizzaBox::MeshRender("SphereModel", new PizzaBox::ColorMaterial(PizzaBox::Color::Yellow)));
-	grapplePoint6->AddComponent(new GrapplePoint(35.0f));
+	grapplePoint6->AddComponent(new GrapplePoint(20.0f));
 		child = CreateObject<PizzaBox::GameObject>(grapplePoint6, PizzaBox::Vector3(0.0f, -3.5f, 0.0f));
 		child->AddComponent(new PizzaBox::PointLight(1.0f, PizzaBox::Color::Yellow, 5.0f));
 
-	// Test Water Object
-	PizzaBox::GameObject* baseWater = CreateObject<PizzaBox::GameObject>(PizzaBox::Vector3(0.0f, 0.0f, 0.0f), PizzaBox::Euler(), PizzaBox::Vector3(70.0f, 20.0f, 70.0f));
+	//Test Water Object
+	PizzaBox::GameObject* baseWater = CreateObject<PizzaBox::GameObject>(PizzaBox::Vector3(0.0f, 0.0f, 0.0f), PizzaBox::Euler(), PizzaBox::Vector3(140.0f, 40.0f, 140.0f));
 	auto water = new PizzaBox::WaterMaterial("WaterTexture","","",32.0f,60.0f);
 	water->SetWaveParamaters(PizzaBox::Vector4(2.0f, 4.0f, 4.0f, 2.0f), PizzaBox::Vector4(0.8f, 0.2f, 0.2f, 0.2f), PizzaBox::Vector4(0.4f, 0.4f, 0.4f, 0.2f));
 	baseWater->AddComponent(new PizzaBox::MeshRender("WaterMesh", water));
 
 
-	// Background Design
-
-	int backgroundbuildingCount = 30;
-	for (int i = 0; i < 30; i++) {
-		PizzaBox::GameObject* backgroundHouse1 = CreateObject<PizzaBox::GameObject>(PizzaBox::Vector3(-400.0f, -5.0f, 400.0f - (i * 250)), PizzaBox::Euler(), PizzaBox::Vector3(50.0f, 150.0f, 50.0f));
+	//Background Design
+	for(int i = 0; i < 30; i++){
+		PizzaBox::GameObject* backgroundHouse1 = CreateObject<PizzaBox::GameObject>(PizzaBox::Vector3(-400.0f, -5.0f, 400.0f - (i * 150.0f)), PizzaBox::Euler(), PizzaBox::Vector3(100.0f, 300.0f, 100.0f));
 		texMat = new PizzaBox::TexturedMaterial("ConcreteTexture", false, "", "", 32.0f, 20.0f);
 		backgroundHouse1->AddComponent(new PizzaBox::MeshRender("CubeModel", texMat));
-		//platform->AddComponent(new PizzaBox::Collider(platform->GetTransform()->GlobalScale())); 
-	}
 
-	for (int i = 0; i < 30; i++) {
-		PizzaBox::GameObject* backgroundHouse2 = CreateObject<PizzaBox::GameObject>(PizzaBox::Vector3(400.0f, -5.0f, 400.0f - (i * 250)), PizzaBox::Euler(), PizzaBox::Vector3(50.0f, 150.0f, 50.0f));
+		PizzaBox::GameObject* backgroundHouse2 = CreateObject<PizzaBox::GameObject>(PizzaBox::Vector3(400.0f, -5.0f, 400.0f - (i * 150.0f)), PizzaBox::Euler(), PizzaBox::Vector3(100.0f, 300.0f, 100.0f));
 		texMat = new PizzaBox::TexturedMaterial("ConcreteTexture", false, "", "", 32.0f, 20.0f);
 		backgroundHouse2->AddComponent(new PizzaBox::MeshRender("CubeModel", texMat));
-		//platform->AddComponent(new PizzaBox::Collider(platform->GetTransform()->GlobalScale())); 
 	}
-
-
 
 	return true;
 }
 
-void Level1::Destroy() {
+void Level1::Destroy(){
 	Scene::Destroy();
 }
