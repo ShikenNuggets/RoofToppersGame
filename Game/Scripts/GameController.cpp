f4189ca3827b4bd1cdae1dfe5ea119cee5c02f12
Carter Rennick <carter.rennick@gmail.com>
2019-03-12 15:12:38 -0400

    Created GameController script to handle respawning the player

---
+++ Game/Scripts/GameController.cpp
@@ -0,0 +1,39 @@
+#include "GameController.h"
+
+#include <Core/SceneManager.h>
+
+#include "Objects/Player.h"
+
+using namespace GamePackage;
+
+GameController::GameController(const PizzaBox::Vector3& pos_, const PizzaBox::Euler& rot_) : player(nullptr), camera(nullptr), spawnPos(pos_), spawnRotation(rot_){
+}
+
+GameController::~GameController(){
+}
+
+void GameController::OnStart(){
+	camera = PizzaBox::SceneManager::CurrentScene()->GetComponentInScene<CameraController>();
+	if(camera == nullptr){
+		PizzaBox::Debug::LogError("No PizzaBox::Camera found in scene!", __FILE__, __LINE__);
+		return;
+	}
+
+	ResetScene();
+}
+
+void GameController::Update(const float deltaTime_){
+}
+
+void GameController::OnDestroy(){
+}
+
+void GameController::ResetScene(){
+	if(player != nullptr){
+		PizzaBox::SceneManager::SceneManager::CurrentScene()->DestroyObject(player);
+		player = nullptr;
+	}
+
+	player = PizzaBox::SceneManager::CurrentScene()->CreateObject<Player>(spawnPos, spawnRotation);
+	camera->SetTarget(player);
+}
