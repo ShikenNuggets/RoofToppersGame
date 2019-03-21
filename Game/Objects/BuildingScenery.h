9c6e810d2ca68da2279a747213f5361f5746d237
Sean Gallagher <seandavidgallagher@gmail.com>
2019-03-21 17:32:04 -0400

    randomized building texture

--- Game/Objects/BuildingScenery.h
+++ Game/Objects/BuildingScenery.h
@@ -6,12 +6,36 @@
 #include <Graphics/Models/MeshRender.h>
 #include <Object/GameObject.h>
 #include <Physics/Collider.h>
-
+#include "Tools/Random.h"
 namespace GamePackage{
 	class BuildingScenery : public PizzaBox::GameObject{
 	public:
 		BuildingScenery(const PizzaBox::Vector3& pos_ = PizzaBox::Vector3(), const PizzaBox::Euler& rot_ = PizzaBox::Euler(), const PizzaBox::Vector3& scale_ = PizzaBox::Vector3(1.0f, 1.0f, 1.0f), bool hasWindows_ = true) : GameObject(pos_, rot_, scale_){
-			AddComponent(new PizzaBox::MeshRender("CubeModel", new PizzaBox::TexturedMaterial("ConcreteTexture", false, "", "", 32.0f, 20.0f)));
+			auto texMaterial = new PizzaBox::TexturedMaterial("ConcreteTexture", false, "", "", 32.0f, 20.0f);
+
+			int randomTexture = PizzaBox::Random::Range(1, 5);
+			if (randomTexture == 1) {
+				texMaterial = new PizzaBox::TexturedMaterial("ConcreteTexture", false, "", "", 32.0f, 20.0f);
+			}
+
+			else if (randomTexture == 2) {
+				texMaterial = new PizzaBox::TexturedMaterial("ConcreteTextureTwo", false, "", "", 32.0f, 20.0f);
+			}
+
+			else if (randomTexture == 3) {
+			
+				texMaterial = new PizzaBox::TexturedMaterial("BrickTexture", false, "", "", 32.0f, 20.0f);
+			}
+
+			else if (randomTexture == 4) {
+				texMaterial = new PizzaBox::TexturedMaterial("BrickTextureTwo", false, "", "", 32.0f, 20.0f);
+			}
+
+			else if(randomTexture == 5) {
+				texMaterial = new PizzaBox::TexturedMaterial("BrickTextureThree", false, "", "", 32.0f, 20.0f);
+			}
+
+			AddComponent(new PizzaBox::MeshRender("CubeModel", texMaterial));
 			SetStatic(true);
 
 			if(hasWindows_){
