0a86e2ffcb8bf5fc3f160891d428eace4a82e3e0
Carter Rennick <carter.rennick@gmail.com>
2019-04-01 14:24:14 -0400

    Disabled shadows on distant buildings

--- Game/Objects/BuildingScenery.h
+++ Game/Objects/BuildingScenery.h
@@ -7,36 +7,33 @@
 #include <Graphics/Models/MeshRender.h>
 #include <Object/GameObject.h>
 #include <Physics/Collider.h>
-#include "Tools/Random.h"
+#include <Tools/Random.h>
 
 namespace GamePackage{
 	class BuildingScenery : public PizzaBox::GameObject{
 	public:
 		BuildingScenery(const PizzaBox::Vector3& pos_ = PizzaBox::Vector3(), const PizzaBox::Euler& rot_ = PizzaBox::Euler(), const PizzaBox::Vector3& scale_ = PizzaBox::Vector3(1.0f, 1.0f, 1.0f), bool hasWindows_ = true, bool hasRandomizedHouseParts_ = true) : GameObject(pos_, rot_, scale_){
-			auto texMaterial = new PizzaBox::TexturedMaterial("BrickTexture", false, "", "", 32.0f, 32.0f);
+			PizzaBox::TexturedMaterial* texMaterial = nullptr;
 
 			int randomTexture = PizzaBox::Random::Range(1, 5);
-			if (randomTexture == 1) {
+			if(randomTexture == 1){
+				texMaterial = new PizzaBox::TexturedMaterial("BrickTexture", false, "", "", 32.0f, 32.0f);
+			}else if(randomTexture == 2){
+				texMaterial = new PizzaBox::TexturedMaterial("BrickTextureTwo", false, "", "", 32.0f, 32.0f);
+			}else if(randomTexture == 3){
+				texMaterial = new PizzaBox::TexturedMaterial("BrickTextureThree", false, "", "", 32.0f, 32.0f);
+			}else if(randomTexture == 4){
+				texMaterial = new PizzaBox::TexturedMaterial("BrickTextureFour", false, "", "", 32.0f, 32.0f);
+			}else if(randomTexture == 5){
+				texMaterial = new PizzaBox::TexturedMaterial("BrickTextureFive", false, "", "", 32.0f, 32.0f);
+			}else{
 				texMaterial = new PizzaBox::TexturedMaterial("BrickTexture", false, "", "", 32.0f, 32.0f);
 			}
 
-			else if (randomTexture == 2) {
-				texMaterial = new PizzaBox::TexturedMaterial("BrickTextureTwo", false, "", "", 32.0f, 32.0f);
-			}
-
-			else if (randomTexture == 3) {
-			
-				texMaterial = new PizzaBox::TexturedMaterial("BrickTextureThree", false, "", "", 32.0f, 32.0f);
-			}
-
-			else if (randomTexture == 4) {
-				texMaterial = new PizzaBox::TexturedMaterial("BrickTextureFour", false, "", "", 32.0f, 32.0f);
-			}
-
-			else if(randomTexture == 5) {
-				texMaterial = new PizzaBox::TexturedMaterial("BrickTextureFive", false, "", "", 32.0f, 32.0f);
-			}
-			AddComponent(new PizzaBox::MeshRender("CubeModel", texMaterial));
+			texMaterial->ReceivesShadows(false);
+			auto mr = new PizzaBox::MeshRender("CubeModel", texMaterial);
+			mr->SetCastsShadows(false);
+			AddComponent(mr);
 			SetStatic(true);
 
 			if(hasWindows_){
@@ -46,7 +43,10 @@ namespace GamePackage{
 					PizzaBox::Vector3(5.0f, 5.0f, 52.0f)
 					);
 				auto window = new PizzaBox::ReflectiveMaterial(true, PizzaBox::ReflectiveMaterial::glass);
-				backgroundHouseWindow->AddComponent(new PizzaBox::MeshRender("BuildingGlassModel", window));
+				window->ReceivesShadows(false);
+				mr = new PizzaBox::MeshRender("BuildingGlassModel", window);
+				mr->SetCastsShadows(false);
+				backgroundHouseWindow->AddComponent(mr);
 
 				auto backgroundHouseWindowSide = PizzaBox::SceneManager::CurrentScene()->CreateObject<PizzaBox::GameObject>(
 					pos_ + PizzaBox::Vector3(15.0f, 105.0f, 0.0f),
@@ -54,10 +54,13 @@ namespace GamePackage{
 					PizzaBox::Vector3(5.0f, 5.0f, 52.0f)
 					);
 				window = new PizzaBox::ReflectiveMaterial(true, PizzaBox::ReflectiveMaterial::glass);
-				backgroundHouseWindowSide->AddComponent(new PizzaBox::MeshRender("BuildingGlassModel", window));
+				window->ReceivesShadows(false);
+				mr = new PizzaBox::MeshRender("BuildingGlassModel", window);
+				mr->SetCastsShadows(false);
+				backgroundHouseWindowSide->AddComponent(mr);
 			}
 
-			if (hasRandomizedHouseParts_) {
+			if(hasRandomizedHouseParts_){
 				int randomHousePart= PizzaBox::Random::Range(1, 3);
 
 				auto HousePartTop = PizzaBox::SceneManager::CurrentScene()->CreateObject<PizzaBox::GameObject>(
@@ -90,37 +93,37 @@ namespace GamePackage{
 					);
 
 				texMaterial = new PizzaBox::TexturedMaterial("ConcreteTexture", false, "", "", 32.0f, 32.0f);
-				HousePartTop->AddComponent(new PizzaBox::MeshRender("CubeModel", texMaterial));
+				texMaterial->ReceivesShadows(false);
+				mr = new PizzaBox::MeshRender("CubeModel", texMaterial);
+				mr->SetCastsShadows(false);
+				HousePartTop->AddComponent(mr);
+
 				texMaterial = new PizzaBox::TexturedMaterial("ConcreteTexture", false, "", "", 32.0f, 32.0f);
-				HousePartBottom->AddComponent(new PizzaBox::MeshRender("CubeModel", texMaterial));
+				texMaterial->ReceivesShadows(false);
+				mr = new PizzaBox::MeshRender("CubeModel", texMaterial);
+				mr->SetCastsShadows(false);
+				HousePartBottom->AddComponent(mr);
 
 
-				if (randomHousePart == 1) {
-
+				if(randomHousePart == 1){
 					texMaterial = new PizzaBox::TexturedMaterial("ConcreteTexture", false, "", "", 32.0f, 32.0f);
-					HousePartChimeny->AddComponent(new PizzaBox::MeshRender("CubeModel", texMaterial));
-
-				}
-
-				else if (randomHousePart == 2) {
-
+					texMaterial->ReceivesShadows(false);
+					mr = new PizzaBox::MeshRender("CubeModel", texMaterial);
+					HousePartChimeny->AddComponent(mr);
+				}else if (randomHousePart == 2){
 					texMaterial = new PizzaBox::TexturedMaterial("ConcreteTexture", false, "", "", 32.0f, 32.0f);
-					HousePartChimeny2->AddComponent(new PizzaBox::MeshRender("CubeModel", texMaterial));
-
-				}
-				else {
-
+					texMaterial->ReceivesShadows(false);
+					mr = new PizzaBox::MeshRender("CubeModel", texMaterial);
+					mr->SetCastsShadows(false);
+					HousePartChimeny2->AddComponent(mr);
+				}else{
 					texMaterial = new PizzaBox::TexturedMaterial("ConcreteTexture", false, "", "", 32.0f, 32.0f);
-					HousePartChimeny3->AddComponent(new PizzaBox::MeshRender("CubeModel", texMaterial));
-
-
+					texMaterial->ReceivesShadows(false);
+					auto mr = new PizzaBox::MeshRender("CubeModel", texMaterial);
+					mr->SetCastsShadows(false);
+					HousePartChimeny3->AddComponent(mr);
 				}
-
-
-
 			}
-
-
 		}
 
 		virtual ~BuildingScenery() override{}
