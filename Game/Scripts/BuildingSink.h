6144725496073c3e7eec84d7b6e257b486d172a0
Sean Gallagher <seandavidgallagher@gmail.com>
2019-04-16 16:48:47 -0400

    updated building sinks collision to work now

--- Game/Scripts/BuildingSink.h
+++ Game/Scripts/BuildingSink.h
@@ -1,25 +1,26 @@
 #ifndef BUILDING_SINK_H
 #define BUILDING_SINK_H
 
-#include <Object/GameObject.h>
+#include "Object/Component.h"
+#include "Object/GameObject.h"
 #include <Script/Script.h>
-namespace GamePackage {
-	class BuildingSink : public PizzaBox::Script {
-	
+
+namespace GamePackage{
+	class BuildingSink : public PizzaBox::Script{	
 	public:
-		BuildingSink(float collapseTime_ = 5.0f);
+		BuildingSink(PizzaBox::Vector3 startPositon_,float collapseTime_ = 5.0f,float collapseSpeed_ = 5.0f);
 		~BuildingSink();
 		virtual void OnStart() override;
 		virtual void Update(const float deltaTime_) override;
 		virtual void OnDestroy() override;
-		virtual void OnCollision(const PizzaBox::GameObject* const other_);
-		private:
+		virtual void OnCollision(const PizzaBox::CollisionInfo& info_);
+	private:
+		PizzaBox::Vector3 startPositon;
 		float collapseSpeed;
 		float collapseTimer;
 		float collapseTime;
 		bool collapse;
 		void BeginDestruction();
 	};
-
 }
 #endif 
